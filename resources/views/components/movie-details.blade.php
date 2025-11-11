f8331da28df092b6317fa1400f3c5fb8972ea438
n00242958 <195749667+n00242958@users.noreply.github.com>
2025-11-11 14:17:36 +0000

    Update movie details and add cast list

--- resources/views/components/movie-details.blade.php
+++ resources/views/components/movie-details.blade.php
@@ -1,6 +1,6 @@
 @props(['movie'])
 
-<div class="border rounded-lg shadow-md p-6 bg-white hover:shadow-lg transition duration:300 max-w-xl mx-auto">
+<div class="p-6">
     <h1 class="font-bold text-black-600 mb-2" style="font-size: 3rem;">{!! $movie->title !!}</h1>
 
     <div class="overflow-hidden rounded-lg mb-4 flex justify-center">
@@ -15,11 +15,43 @@
         Age Rating: {{ $movie->age_rating }}
     </h2>
 
-    <h3 class="text-gray-800 font-semibold mb-2" style="font-size: 2rem;">
+    @if (auth()->user()->role == 'admin')
+        {{-- edit/delete buttons --}}
+        <div class="mt-4 flex space-x-2">
+            <a href="{{ route('movies.edit', $movie) }}">
+                Edit
+            </a>
+            <form action="{{ route('movies.destroy', $movie) }}" method="POST"
+            onsubmit="return confirm('Are you sure you wish to delete this movie?');">
+                @csrf
+                @method('DELETE')
+                <button type="submit">
+                    Delete
+                </button>
+            </form>
+        </div>
+    @endif
+
+    <h4 class="font-semibold text-md mt-8">
         Description
-    </h3>
+    </h4>
 
     <p class="text-gray-700 leading-relaxed">
         {!! $movie->description !!}
     </p>
+
+    {{-- castings --}}
+    <h4 class="font-semibold text-md mt-8">Cast</h4>
+    @if($movie->castings->isEmpty())
+        <p class="text-gray-600">No cast members added yet.</p>
+    @else
+        <ul class="mt-4 space-y-4">
+            @foreach($movie->castings as $casting)
+                <li class="bg-gray-100 p-4 rounded-lg">
+                    <p>{{ $casting->person }}</p>
+                    <p>{{ $casting->role }}</p>
+                </li>
+            @endforeach
+        <ul>
+    @endif
 </div>
