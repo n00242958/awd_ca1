da67ba1866c4c24448e969df21e157736907fe70
n00242958 <195749667+n00242958@users.noreply.github.com>
2025-11-11 14:21:15 +0000

    Fix indent

--- resources/views/movies/show.blade.php
+++ resources/views/movies/show.blade.php
@@ -8,10 +8,10 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    {{-- Display movie details component --}}
-                    <x-movie-details
-                        :movie="$movie"
-                    />
+                {{-- Display movie details component --}}
+                <x-movie-details
+                    :movie="$movie"
+                />
             </div>
         </div>
     </div>
