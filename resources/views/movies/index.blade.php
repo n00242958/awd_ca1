74e94106709b8fe4accf3acfb5301c7c92d682aa
n00242958 <195749667+n00242958@users.noreply.github.com>
2025-11-11 14:57:33 +0000

    Update most components to use dark UI

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        <h2 class="font-semibold text-xl text-white leading-tight">
             @if(isset($search))
                 {{ __('Movies matching') . ' \'' }}{!! e($search) !!}{{ '\'' }}
             @else
