fde0e2bf0be4535df33bc35a56787f6137be6a81
n00242958 <195749667+n00242958@users.noreply.github.com>
2025-11-25 14:14:33 +0000

    Create WatchListDetails

--- resources/views/watch_lists/show.blade.php
+++ resources/views/watch_lists/show.blade.php
@@ -1,3 +1,18 @@
-<div>
-    <!-- The biggest battle is the war against ignorance. - Mustafa Kemal Atatürk -->
-</div>
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-white leading-tight">
+            {{ __('Watch List Details') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                {{-- Display watch list details component --}}
+                <x-watch-list-details
+                    :watch_list="$watch_list"
+                />
+            </div>
+        </div>
+    </div>
+</x-app-layout>
